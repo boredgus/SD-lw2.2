fd8b3ffb725e0b84f2483beaf8619b4427ba9d28
boredgus <daha.kyiv@gmail.com>
2020-12-30 01:36:25 +0200

    I just wanna sleep

--- base/Main.java
+++ base/Main.java
@@ -1,24 +1,13 @@
 package base;
 
-
 import DAO.BusDAO;
 import DTO.BusDTO;
 
 import java.util.ArrayList;
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
-
 
 public class Main {
 
     public static void main(String[] args) {
-//        Locale.setDefault(new Locale("en", "US"));
-//        ResourceBundle myMessages = ResourceBundle.getBundle("resources.MyMessages en US");
-//        System.out.print(myMessages.getString("ErrorKEY"));
-//        ResourceBundle.getBundle("resources.MyMessages en US").getString("ErrorKEY");
-//        System.out.println(Locale.getDefault());
         try {
             Menu menu = new Menu();
             menu.setLanguage();
@@ -30,33 +19,6 @@ public class Main {
 
 
 
-
-//        Scanner in = new Scanner(System.in);
-//        boolean check = true;
-//        System.out.print("ДОБРО ПОЖАЛОВАТЬ!\nВыберите язык:\n1. English\n2. Русский (по умолчанию)");
-//        String language = in.nextLine();
-//        String message = "";
-//        while(check) {
-//            System.out.print(message + "\nChoose appropriate option:\n" +
-//                    "1. Add row\n2. Update row\n3. Delete row\4. Exit database\n");
-//            String option = in.nextLine();
-//            switch(option){
-//                case "1":
-//                    boolean result = menu.addRow();
-//                    if(result)
-//                    break;
-//                case "2":
-//
-//                    break;
-//                case "3":
-//
-//                    break;
-//                default:
-//                    message = "Wrong option! Try again.";
-//            }
-//        }
-
-//Locale.setDefault(new Locale("en", "US"));
 //    BusDAO dao = new BusDAO();
 //     BusDTO dto = new BusDTO();
 //        // створюю список елементів, вигляд та контролер елементів
@@ -90,6 +52,5 @@ public class Main {
 //        // видаляю деякі елементи з БД
 //        dao.deleteRow(4);
 //        dao.deleteRow(6);
-
     }
 }
--- base/Menu.java
+++ base/Menu.java
@@ -1,21 +1,22 @@
 package base;
 
-import java.util.Enumeration;
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.Scanner;
+import java.util.*;
+
 import DAO.BusDAO;
 import DTO.BusDTO;
 
 public class Menu {
     private BusDAO dao = new BusDAO();
     private BusDTO dto = new BusDTO();
+    ArrayList<Bus> busList = new ArrayList<>();
+    BusView busView = new BusView();
+    BusController busController = new BusController(busList, busView);
     ResourceBundle bundle = ResourceBundle.getBundle("resources.MyMessages " + Locale.getDefault());
 
     public void setLanguage(){
         Scanner in = new Scanner(System.in);
         boolean check = true;
-        System.out.print("ДОБРО ПОЖАЛОВАТЬ!\n");
+        System.out.print(bundle.getString("WelcomeKEY")+"\n");
         String message = "";
         while(check) {
             System.out.print(message + "Выберите язык:\n1. English\n2. Русский (по умолчанию)\n");
@@ -38,45 +39,65 @@ public class Menu {
     public void start(){
         Scanner in = new Scanner((System.in));
         boolean check = true;
-        String
+        String message = "";
         while(check){
-            System.out.print(bundle.getString("MenuKEY"));
+            System.out.print(message + bundle.getString("Menu1KEY"));
+            message = "";
             String option = in.nextLine();
             switch(option){
                 case "1":
-                    boolean result = addRow();
-                    if(result)
+                    addRow();
                     break;
                 case "2":
-
+                    updateRow();
                     break;
                 case "3":
-
+                    deleteRow();
+                    break;
+                case "4":
+                    check = false;
                     break;
                 default:
-                    String message = "Wrong option! Try again.";
-                    message = (check) ?  "" : "0";
+                    message = bundle.getString("WrongOptionKEY");
             }
         }
         in.close();
     }
 
-    public boolean addRow(){
+    public void addRow(){
         Scanner in = new Scanner(System.in);
+        System.out.print(bundle.getString("AddingMessageKEY"));
+        System.out.print(bundle.getString("EnterDriverNameKEY"));
+        String driverName = in.nextLine();
+        System.out.print(bundle.getString("EnterRouteKEY"));
+        String route = in.nextLine();
+        System.out.print(bundle.getString("EnterNumKEY"));
+        int num = in.nextInt();
+        System.out.print(bundle.getString("EnterMileageKEY"));
+        long mileage = in.nextLong();
 
+        busList.add(new Bus(driverName, num, route, mileage));
+        dto.setAll(busController.getWholeBus(busList.size() - 1));
+        dao.addRow(dto);
         in.close();
-        return true;
     }
 
     public void updateRow(){
         Scanner in = new Scanner(System.in);
+        System.out.print(bundle.getString("UpdatingMessageKEY"));
 
         in.close();
     }
 
     public void deleteRow(){
         Scanner in = new Scanner(System.in);
-
+        System.out.print(bundle.getString("DeletingMessageKEY"));
+        boolean check = true;
+        while(check){
+            System.out.print(bundle.getString("EnterIdKEY"));
+            int id = in.nextInt();
+            dao.deleteRow(id);
+        }
         in.close();
     }
 }
